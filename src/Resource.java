7dc25834682a11e92e98e9bc240492c3676bb8ea
william <454030959@qq.com>
2019-03-21 13:01:37 +0800

    提交Resource.java

---
+++ src/Resource.java
@@ -0,0 +1,14 @@
+
+public class Resource {
+	final static boolean NEXCLUDED = false;
+	final static boolean EXCLUDED = true;
+	final static int READY=-1;
+	final static int SUCCESS=1;
+	final static int FAIL=0;
+	final static int LCASES=256;
+	final static int UCASES=257;
+	final static int NUM=258;
+	final static int EPSILON=259;
+	final static int ANY=260;
+	final static int WS=261;
+}
